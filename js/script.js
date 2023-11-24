1117b99f2c89f40061975c9812c1515c9c2e2219
Paulo Barros <barrosp335@gmail.com>
2023-11-24 17:43:42 -0300

    Adicionado contador de pulo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+// const mario = document.querySelector(".mario");
+// const pipe = document.querySelector(".pipe");
+// const jumpButton = document.getElementById("jumpButton");
+// const jumpCounter = document.getElementById("jumpCounter");
+
+// let isJumping = false;
+// let jumps = 0;
+
+// const jump = () => {
+//   if (!isJumping) {
+//     isJumping = true;
+//     mario.classList.add("jump");
+//     jumps++;
+//     updateJumpCounter();
+
+//     setTimeout(() => {
+//       mario.classList.remove("jump");
+//       isJumping = false;
+//     }, 400);
+//   }
+// };
+
+// jumpButton.addEventListener("click", jump);
+
+// const updateJumpCounter = () => {
+//   jumpCounter.textContent = `Jumps: ${jumps}`;
+// };
+
+// const loop = setInterval(() => {
+//   const pipePosition = pipe.offsetLeft;
+//   const marioPosition = +window
+//     .getComputedStyle(mario)
+//     .bottom.replace("px", "");
+
+//   console.log(marioPosition);
+
+//   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+//     pipe.style.animation = "none";
+//     pipe.style.left = `${pipePosition}px`;
+
+//     mario.style.animation = "none";
+//     mario.style.bottom = `${marioPosition}px`;
+
+//     mario.src = "./images/game-over.png";
+//     mario.style.width = "75px";
+//     mario.style.marginLeft = "50px";
+
+//     clearInterval(loop);
+//   }
+// }, 10);
+
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const jumpButton = document.getElementById("jumpButton");
@@ -20,13 +71,13 @@ const jump = () => {
   }
 };
 
-jumpButton.addEventListener("click", jump);
+jumpButton.addEventListener("touchstart", jump); // Usar touchstart em vez de click
 
 const updateJumpCounter = () => {
   jumpCounter.textContent = `Jumps: ${jumps}`;
 };
 
-const loop = setInterval(() => {
+const loop = () => {
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window
     .getComputedStyle(mario)
@@ -45,9 +96,13 @@ const loop = setInterval(() => {
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
 
-    clearInterval(loop);
+    cancelAnimationFrame(loop); // Para a animação
+  } else {
+    requestAnimationFrame(loop); // Continua a animação
   }
-}, 10);
+};
+
+loop(); // Inicia o loop principal
 
 // // document.addEventListener("keydown", jump);
 // document.getElementById("jumpButton").addEventListener("click", jump);
